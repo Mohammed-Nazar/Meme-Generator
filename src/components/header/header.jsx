b5cecad95e3268028fe6bfc92dc37aa752703596
Mohammed-Nazar <x3raqe@gmail.com>
2023-07-09 20:38:00 +0300

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+
+export default function Header(){
+    return (
+        <>
+        <header>
+        <img src="../../images/troll-face.png" alt="troll face" />
+        <h2>Meme Generator</h2>
+        </header>
+        </>
+    )
+}
